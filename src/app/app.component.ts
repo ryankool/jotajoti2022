d8398ed5d0b300805a607eec7ed5c31591597f8f
Ryan Kool <ryankool@sydl1507587.oc.rabonet.com>
2022-09-29 14:06:52 +0200

    added coordinaten

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { min } from 'rxjs';
+import { questions } from './questions';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'jotajoti2022';
+export class AppComponent implements OnInit {
+  team: string | null = null;
+  post: string | null = null;
+  question: string = '';
+  coordinaat: string = '';
+  answer: string = '';
+  correctAnswer: string = '';
+  hint: string = '';
+  isValid: boolean = false;
+  answerSubmitted: boolean = false;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.team = params['team'];
+      this.post = params['post'];
+      this.getQuestion();
+    });
+  }
+
+  private getQuestion() {
+    let myarray: any = [];
+    questions.forEach((question) => {
+      myarray.push(
+        `https://jotajoti2022.web.app/?team=${question.team}&post=${question.post}`
+      );
+      if (question.team === this.team && question.post === this.post) {
+        this.question = question.question;
+        this.correctAnswer = question.answer;
+        this.hint = question.hint;
+        this.coordinaat = question.coordinaat;
+      }
+    });
+    console.log(myarray);
+  }
+
+  public validateAnswer() {
+    if (this.answer.toLowerCase().includes(this.correctAnswer)) {
+      this.isValid = true;
+    } else {
+      this.isValid = false;
+    }
+    this.answerSubmitted = true;
+  }
 }
